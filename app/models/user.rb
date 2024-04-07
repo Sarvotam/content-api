d7cccae233c7dc9ddb65a1538c8ae25924410d82
Sarvotam <hengajusarvottam@gmail.com>
2024-04-07 11:52:00 +0545

    authorization using jti for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable,
           :jwt_authenticatable, jwt_revocation_strategy: self
+
+  def jwt_payload
+    super
+  end
 end
